40ade6c3518f53fa72af35f5066268caf3d21c91
Altanis <38045884+Altanis@users.noreply.github.com>
2024-09-11 19:46:32 -0400

    increment zobrist key

--- src/utils/board.rs
+++ src/utils/board.rs
@@ -517,8 +517,28 @@ impl Board {
     pub fn make_null_move(&self) -> Board {
         let mut board = self.clone();
 
+        if let Some(ep) = board.en_passant {
+            board.zobrist_key ^= ZOBRIST_EN_PASSANT_KEYS[ep.rank as usize + 1];
+        } else {
+            board.zobrist_key ^= ZOBRIST_EN_PASSANT_KEYS[0];
+        }
+    
+        if board.side_to_move == PieceColor::Black {
+            board.zobrist_key ^= ZOBRIST_SIDE_TO_MOVE;
+        }
+
         board.side_to_move = !board.side_to_move;
         board.en_passant = None;
+
+        if let Some(ep) = board.en_passant {
+            board.zobrist_key ^= ZOBRIST_EN_PASSANT_KEYS[ep.rank as usize + 1];
+        } else {
+            board.zobrist_key ^= ZOBRIST_EN_PASSANT_KEYS[0];
+        }
+    
+        if board.side_to_move == PieceColor::Black {
+            board.zobrist_key ^= ZOBRIST_SIDE_TO_MOVE;
+        }
 
         board
     }
